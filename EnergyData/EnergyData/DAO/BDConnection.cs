b514124c092e335f48c7f1d034c9e9118ca63b2f
Gabriel Estavaringo <gabriel.estavaringo@gmail.com>
2018-09-14 00:23:57 -0300

    Included connection with MySQL DB

--- EnergyData/EnergyData/DAO/BDConnection.cs
+++ EnergyData/EnergyData/DAO/BDConnection.cs
@@ -3,8 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace EnergyData.DAO {
     class BDConnection {
+        String server, database, user, pwd;
+        MySqlConnection connection = null;
+
+        public BDConnection(String server, String database, String user, String pwd) {
+            this.database = database;
+            this.pwd = pwd;
+            this.user = user;
+            this.server = server;
+        }
+
+        public MySqlConnection connect() {
+            if(connection == null) {
+                this.connection = new MySqlConnection("Server=" + server + ";Database=" + database + ";Uid=" + user + ";Pwd=" + pwd);
+            } else {
+                Console.WriteLine("Data: " + DateTime.Now + "Conexão já aberta ");
+                throw new Exception("Conexão já aberta ");
+            }
+            return this.connection;
+        }
+
     }
 }
